04319cf92eff0163c2dba10a3d0ef3c8e909e954
ldss-jm <ldss-jm@users.noreply.github.com>
2017-05-19 09:28:12 -0400

    update w/revised postgres_connect

--- 773_field_group_tag.rb
+++ 773_field_group_tag.rb
@@ -1,4 +1,6 @@
 load '../postgres_connect/connect.rb'
 
-results = make_query('773_field_group_tag.sql')
-write_results('773_field_group_tag.results.txt', results, 'tsv')
+c = Connect.new
+
+c.make_query('773_field_group_tag.sql')
+c.write_results('773_field_group_tag.results.txt', format: 'tsv')
--- mono_bibs_unsupp_holdings_endeca_display_probs.rb
+++ mono_bibs_unsupp_holdings_endeca_display_probs.rb
@@ -1,13 +1,13 @@
 load '../postgres_connect/connect.rb'
 
+c = Connect.new
 
 outfile = 'mono_bibs_unsupp_holdings_endeca_display_probs.xlsx'
 headers = ['bnum', 'cnum', 'problem_location', 'url']
-results = make_query('mono_bibs_unsupp_holdings_endeca_display_probs.sql')
-write_results(outfile,
-              results,
-              headers,
-              'xlsx'
+c.make_query('mono_bibs_unsupp_holdings_endeca_display_probs.sql')
+c.write_results(outfile,
+              headers: headers,
+              format: 'xlsx'
 )
 
 
@@ -16,13 +16,14 @@ write_results(outfile,
 email_body = 'Report attached. These holdings records are unsuppressed and' +
 ' have no items attached. They are attached to bibs that display holdings in' +
 ' Endeca. This causes display problems.'
-email_address = get_email_secret(@email_secret)
+email_address = c.yield_email
 
-mail_results(outfile,
+c.mail_results(outfile,
             mail_details = {:from => email_address,
                             :to => email_address,
                             :subject => 'Mono bibs w/unsuppressed holdings -' +
                                         ' Endeca display problems',
-                            :body => email_body}
+                            :body => email_body},
+            remove_file: true
 )
   
