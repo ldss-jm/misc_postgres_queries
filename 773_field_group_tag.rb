e6a6ed36477b7de611eeeb8f5621eb754e87a1a2
ldss-jm <ldss-jm@users.noreply.github.com>
2017-06-29 14:24:32 -0400

    explicitly exclude headers when writing 773_field group

--- 773_field_group_tag.rb
+++ 773_field_group_tag.rb
@@ -3,4 +3,4 @@ load '../postgres_connect/connect.rb'
 c = Connect.new
 
 c.make_query('773_field_group_tag.sql')
-c.write_results('773_field_group_tag.results.txt', format: 'tsv')
+c.write_results('773_field_group_tag.results.txt', format: 'tsv', include_headers: false)
