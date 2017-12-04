02591d68a3a592794729715f1462e36d85f63f42
ldss-jm <ldss-jm@users.noreply.github.com>
2017-12-04 14:26:59 -0500

    add spr_dupe

---
+++ spr_dupe.rb
@@ -0,0 +1,31 @@
+require_relative '../postgres_connect/connect.rb'
+
+c = Connect.new
+
+query = <<-SQL
+SELECT 'b' || rm.record_num || 'a' as bnum,
+(select STRING_AGG(v.field_content, ';')
+   from sierra_view.varfield v
+   where v.record_id = rm.id
+   and v.marc_tag = '001') as m001,
+(select STRING_AGG(sf.content, ';')
+   from sierra_view.subfield sf
+   where sf.record_id = rm.id
+   and sf.marc_tag = '856'
+   and sf.tag = 'u') as "856|u",
+(select STRING_AGG(sf.content, ';')
+   from sierra_view.subfield sf
+   where sf.record_id = rm.id
+   and sf.marc_tag = '956'
+   and sf.tag = 'u') as "956|u"
+ FROM sierra_view.bib_record b
+ INNER JOIN sierra_view.varfield v on v.record_id = b.id
+ INNER JOIN sierra_view.record_metadata rm on rm.id = b.id
+ WHERE v.marc_tag = '773'
+      and v.field_content ilike '%|tSpringer e-books (online collection)%'
+SQL
+
+c.make_query(query)
+c.write_results('spr_data.txt', format: 'tsv', include_headers: true)
+
+load '../Cataloging_Scripts/springer_dedupe_on_urls.rb'
